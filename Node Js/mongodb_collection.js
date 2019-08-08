6b7d71965a16f3f72ba5659f2bfa9f1704944b3e
RakeshT <rakesh.cse548@gmail.com>
2019-08-08 15:54:24 +0530

    " Node js Basics"

---
+++ Node Js/mongodb_collection.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:8080/";
+MongoClient.connect(url, function(err,db)
+{
+	if(err) throw err;
+	var dbo = db.db("mydb");
+	dbo.createCollection("customers", function(err, res)
+	{
+		if(err) throw err;
+		console.log("collection created");
+		db.close();
+	});
+});
